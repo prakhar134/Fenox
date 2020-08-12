0753ff48624885bc92084bf2f2b74d41edf0fa6d
Prakhar Pandey <prakharpandey.13@gmail.com>
2020-08-12 23:03:24 +0530

    project name changed

--- Fenox/asgi.py
+++ Fenox/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for msinfragroup project.
+ASGI config for Fenox project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'msinfragroup.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Fenox.settings')
 
 application = get_asgi_application()
--- Fenox/wsgi.py
+++ Fenox/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for msinfragroup project.
+WSGI config for Fenox project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'msinfragroup.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Fenox.settings')
 
 application = get_wsgi_application()
